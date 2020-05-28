c18a97fbd2f7b45e52aae52ff078460ce8960ece
yavyletkov <yavyletkov@ya.ru>
2020-05-28 21:16:03 +0300

    Updates

---
+++ precache-manifest.6f455efb3bd571912d91bd379b51d6ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6878a2178ab2fa6d8b95b887f679c017",
+    "url": "/moviesapiapp/index.html"
+  },
+  {
+    "revision": "c21ae1da47d63039fb06",
+    "url": "/moviesapiapp/static/css/main.f101178f.chunk.css"
+  },
+  {
+    "revision": "a34dd9317ba092b75a44",
+    "url": "/moviesapiapp/static/js/2.9ab01f12.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moviesapiapp/static/js/2.9ab01f12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21ae1da47d63039fb06",
+    "url": "/moviesapiapp/static/js/main.28f5dedd.chunk.js"
+  },
+  {
+    "revision": "761d7ee9b54c10788702",
+    "url": "/moviesapiapp/static/js/runtime-main.6525f9ef.js"
+  }
+]);
